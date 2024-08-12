003d0af890b00496bc2fb987d12c4060d5e0e99f
Yohannes Mengistie <yohannesmengistie634@gmail.com>
2024-08-12 21:06:20 +0300

    testing usecase

--- on-boarding/mobile -[Yohannes Mengistie]/task_8/seting_up_linter_rule/lib/feature/e_commerce_app/domain/entities/e_commerce.dart
+++ on-boarding/mobile -[Yohannes Mengistie]/task_8/seting_up_linter_rule/lib/feature/e_commerce_app/domain/entities/e_commerce.dart
@@ -1,24 +1,30 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-class Product extends Equatable{
-      final String name;
-      final String category;
-      final String description;
-      final int price;
-      final int id;
-      final String imageUrl;
 
-      Product({required this.name,required this.category,required this.description,required this.price,required this.id,required this.imageUrl});
-      
-        @override
-        // TODO: implement props
-        List<Object?> get props => [
-          name,
-          category,
-          description,
-          price,
-          id,
-        ];
-      
+class Product extends Equatable {
+  final String name;
+  final String category;
+  final String description;
+  final int price;
+  final int id;
+  final String imageUrl;
+
+  Product(
+      {required this.name,
+      required this.category,
+      required this.description,
+      required this.price,
+      required this.id,
+      required this.imageUrl});
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [
+        name,
+        category,
+        description,
+        price,
+        id,
+      ];
 }
